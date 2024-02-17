e77b815aa5711cd98d44b39c9a39dce90e907625
Magomed Babaev <mbabaev@banki.ru>
2024-02-17 12:14:04 +0300

    add https for local

--- tg-bot-client/vite.config.js
+++ tg-bot-client/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { readFileSync } from "fs";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    host: "tg-bot.dev",
+    https: {
+      key: readFileSync(".cert/key.pem"),
+      cert: readFileSync(".cert/cert.pem"),
+    },
+  },
 });
